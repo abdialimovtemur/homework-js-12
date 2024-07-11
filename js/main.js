640bc6e421467c65fb00ad8f828b4ecafc48efc1
abdialimovtemur <abdialimovtemur@gmail.com>
2024-07-11 09:58:52 +0500

    Commit title

--- js/main.js
+++ js/main.js
@@ -21,10 +21,10 @@
 
 
 
+
+
+
 // 2. Array elementlari kvadratlaridan hosil bo’lgan array hosil qiling. Input: [1, 2, 3, 4, 5] Output: [1, 4, 9, 16, 25]
-
-
-
 
 
 // let arr = [1, 2, 3, 4, 5, 9];
@@ -34,6 +34,10 @@
 //     newArr.push(Math.pow(element, 2))
 // }) 
 // console.log(newArr);
+
+
+
+
 
 
 // 3.Arraydagi musbat sonlar yig’indisini hisoblang. (filter va reduce). Input: [ 1, -4, 12, 0, -3, 29, -150] Output: 42
@@ -49,63 +53,109 @@
 // positeveSum()
 
 
+
+
+
+
 // 4. Stringdagi so’zlarning bosh harflarini oling. (split, map, join). Input: 'George Raymond Richard Martin'. Output: 'GRRM' 
 
 
 // let userStr = prompt("Gapni kiriting")
 
-// function firstLetters() {
-//     let onlyLettersArray = userStr.split('').filter(char => /[a-zA-Z]/.test(char));
-//     console.log(onlyLettersArray)
+// function strLength() {
+//     let arr = userStr.split(" ");
+//     let newArr = [];
+
+//     arr.forEach(element => {
+//         console.log(userStr.indexOf(0))
+//         // newArr.push(element.length)
+//     })
+//     console.log(newArr);
 
 // }
+// strLength()
 
-// firstLetters()
+
+
 
 
 // 5. Berilgan stringni faqatgina harflardan iborat ekanligiga tekshiring
 
-// let userStr = prompt("Gapni kiriting")
-// function isNanFunction() {
+// let userStr = prompt("So'zni kiriting kiriting")
+
+// function strLength() {
+//     let arr = userStr.split(" ");
+//     let newArr = [];
 //     let num = 0;
 //     let str = 0;
-//     for (let i = 0; i < userStr.length; i++) {
+//     arr.forEach(element =>{
+//         for (let i = 0; i < element.length; i++) {
+//             if(!isNaN(element[i])){
+//                 num++
+//             }else{
+//                 str++
+//             }
 
-//         if (isNaN(userStr[i]) == true)
-//         {
-//             str++
 //         }
-//         else {
-//             num++
-//         }
+//     })
 
-//     }
-//     if(num == 0){
-//         console.log("So'z faqat stringdan iborat")
+//     if (num < 1) {
+//         console.log("Gap faqat stringdan iborat")
 //     }else{
-//         console.log("So'zda son bor")
+//         console.log("Gapda raqam mavjud")
 //     }
 
 // }
-
-// isNanFunction()
-
+// strLength()
 
 
-// 6. String berilgan. Stringdagi so'zlar uzunligidan iborat bo'lgan array qaytaring. (split, map) Input: "Hello world" Output: [3, 9, 12] 
+
+// 6. String berilgan. Stringdagi so'zlar uzunligidan iborat bo'lgan array qaytaring. (split, map) Input: "Hello world" Output: [3, 9, 12]
 
 
-let userStr = prompt("Gapni kiriting")
-function strLength() {
-    let arr = userStr.split(" ");
-    console.log(arr)
-// let newArr = [];
+// let userStr = prompt("Gapni kiriting")
+// function strLength() {
+//     let arr = userStr.split(" ");
+//     let newArr = [];
 
-// arr.forEach(element =>{
-//     newArr.push(Math.pow(element, 2))
-// }) 
-// console.log(newArr);
+//     arr.forEach(element =>{
+//         newArr.push(element.length)
+//     })
+//     console.log(newArr);
+
+// }
+// strLength()
+
+
+
+
+// 7. Stringni bo'sh joy bor yoki yo'qligini tekshiring. Input: "salom dunyo" Output: true 
+
+
+// let userStr = ["Samsung", "Apple", "Tesla", '' , "Artel"];
+
+// function IncludeStr() {
+//     console.log(userStr.includes(""))
+// }
+
+// IncludeStr()
+
+
+
+// 8. Objectning kalit va qiymatlarining string ko'rinishidagi yig'indisidan iborat array qaytaring Input: {a: 2, b: 5, c: 7}. Output: ['a2', 'b5', 'c7']
+
+
+
+function objKeyValueToArr(params){ 
+    
+const object1 = {
+    a: 'Temurbek',
+    b: 'Abdialimov',
+};
+
+for (const [key, value] of Object.entries(object1)) {
+    console.log(`${key}: ${value}`);
+
 
 }
-
-strLength()
+{
